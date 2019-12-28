d414db3f4031e623a0aa75c06f75336c22f4b57c
J. Bromley <jay@ai.co>
2019-12-28 00:10:33 -0800

    Cmopleted word-count exercise.

--- clojure/word-count/src/word_count.clj
+++ clojure/word-count/src/word_count.clj
@@ -1,5 +1,20 @@
-(ns word-count)
+(ns word-count
+  (:require [clojure.string :as s]))
 
-(defn word-count [s] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn clean-string [s]
+  (s/join "" (filter #(or (Character/isLetter %1)
+                          (Character/isSpace %1)
+                          (Character/isDigit %1)) s)))
+
+(defn word-count [words]
+  (->> words
+       (s/lower-case)
+       (clean-string)
+       (#(s/split %1 #"[ ]+"))
+       (sort)
+       (partition-by identity)
+       (mapcat (juxt first count))
+       (apply hash-map)))
+
+(defn word-count-2 [words]
+  (frequencies (re-seq #"\w+" (s/lower-case words))))
