287d5eb75a504cfd5cc6c007dc7607765898b011
J. Bromley <jay@ai.co>
2020-01-06 15:27:21 -0800

    Simply if statement by using when.
    Instead of explicitly returning nil for different length strands, use when which
    will return nil when the condition is false.

--- clojure/hamming/src/hamming.clj
+++ clojure/hamming/src/hamming.clj
@@ -1,6 +1,5 @@
 (ns hamming)
 
 (defn distance [strand1 strand2]
-  (if (not= (count strand1) (count strand2))
-    nil
+  (when (= (count strand1) (count strand2))
     (count (filter false? (map = strand1 strand2)))))
