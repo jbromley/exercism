de554b89c282e9c057f8ae7ff1b5273d00ea28bb
J. Bromley <jay@ai.co>
2020-01-07 16:35:49 -0800

    Simplify using clojure.string/replace and condp/re-matches.
    Use replace instead of filtering a known list of invalid characters. Also use
    condp and regexes to make the valid phone numbers more obvious.

--- clojure/phone-number/src/phone_number.clj
+++ clojure/phone-number/src/phone_number.clj
@@ -1,22 +1,15 @@
 (ns phone-number)
 
-(defn clean [string]
-  (apply str (remove #((set "+()-. ") %1) string)))
-
 (defn number [num-string]
-  (let [num (clean num-string)
-        num-len (count num)]
-    (cond
-      (= num-len 10) num
-      (and (= num-len 11) (= (first num) \1)) (subs num 1)
-      :default "0000000000")))
+  (let [digits (clojure.string/replace num-string #"[^\d]" "")]
+    (condp re-matches digits
+      #"^[\d]{10}$" digits
+      #"^1[\d]{10}$" (subs digits 1)
+      "0000000000")))
 
 (defn area-code [num-string]
   (subs (number num-string) 0 3))
 
 (defn pretty-print [num-string]
   (let [num (number num-string)]
-    (format "(%s) %s-%s"
-            (subs num 0 3)
-            (subs num 3 6)
-            (subs num 6))))
+    (format "(%s) %s-%s" (subs num 0 3) (subs num 3 6) (subs num 6))))
