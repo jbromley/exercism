ff1cf4f5c046d17a984ec984b5c8413a5fb459a0
J. Bromley <jay@ai.co>
2020-04-29 17:17:00 -0700

    Initial attempt at the Elixir word_count exercise.
    Seems good except for all the converting from String to codepoints
    and then back.

--- elixir/word-count/lib/word_count.ex
+++ elixir/word-count/lib/word_count.ex
@@ -6,5 +6,28 @@ defmodule WordCount do
   """
   @spec count(String.t()) :: map
   def count(sentence) do
+    sentence
+    |> String.downcase()
+    |> String.codepoints()
+    |> Enum.filter(&is_valid_char?(&1))
+    |> List.to_string()
+    |> String.split(~r/[ _]/, trim: true)
+    |> count_aux(%{})
+  end
+
+  defp count_aux([word | rest], freqs) do
+    case Map.get(freqs, word) do
+      nil ->
+	count_aux(rest, Map.put(freqs, word, 1))
+      val ->
+	count_aux(rest, Map.put(freqs, word, val + 1))
+    end
+  end
+  defp count_aux([], freqs) do
+    freqs
+  end
+
+  def is_valid_char?(char) do
+    Regex.match?(~r/[[:alnum:] _-]/u, char)
   end
 end
