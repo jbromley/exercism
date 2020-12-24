f370b7c99443b06303e7e343f9d397f747b66c65
J. Bromley <jbromley@gmail.com>
2020-12-24 01:02:54 -0800

    Accumulate Roman number in a string.
    Used a string so we would not have to reverse the string at the end.

--- elixir/roman-numerals/lib/roman_numerals.ex
+++ elixir/roman-numerals/lib/roman_numerals.ex
@@ -1,22 +1,22 @@
 defmodule RomanNumerals do
-  @sym_table [{1000, "M"}, {900, "MC"}, {500, "D"}, {400, "DC"}, {100, "C"}, {90, "CX"}, {50, "L"}, {40, "LX"}, {10, "X"}, {9, "XI"}, {5, "V"}, {4, "VI"}, {1, "I"}] 
+  @sym_table [{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}] 
 
   @doc """
   Convert the number to a roman number.
   """
   @spec numeral(pos_integer) :: String.t()
   def numeral(number) do
-    numeral_aux(number, @sym_table, [])
+    numeral_aux(number, @sym_table, "")
   end
   defp numeral_aux(0, _sym_list, n_roman) do
-    n_roman |> List.to_string() |> String.reverse()
+    n_roman
   end
   defp numeral_aux(n, symbol_list, n_roman) do
     {val, symbol} = hd(symbol_list)
     if n < val do
       numeral_aux(n, tl(symbol_list), n_roman)
     else 
-      numeral_aux(n - val, symbol_list, [symbol | n_roman])
+      numeral_aux(n - val, symbol_list, n_roman <> symbol)
     end
   end
 end
