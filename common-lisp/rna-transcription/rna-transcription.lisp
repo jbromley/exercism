a129b80ca070267cda3f90ac921d40dda9a3f3ec
J. Bromley <jay@appliedinvention.com>
2021-02-12 17:28:45 -0800

    Use constant for transcription map.
    Changed the transcription map to be defined with defconstant
    rather than defparameter.

--- common-lisp/rna-transcription/rna-transcription.lisp
+++ common-lisp/rna-transcription/rna-transcription.lisp
@@ -4,14 +4,15 @@
   (:export #:to-rna))
 (in-package #:rna-transcription)
 
-(defparameter *transcription-map* '((#\A . #\U)
-                                    (#\C . #\G)
-                                    (#\G . #\C)
-                                    (#\T . #\A)))
+(defconstant +transcription-map+ '((#\A . #\U)
+                                   (#\C . #\G)
+                                   (#\G . #\C)
+                                   (#\T . #\A)))
+
 (defun to-rna-base (base)
   "Transcribe a single DNA base to the RNA complement. Return NIL if the
 base is not a valid base (A, C, G, T)."
-  (cdr (assoc base *transcription-map*)))
+  (cdr (assoc base +transcription-map+)))
 
 (defun to-rna (str)
   "Transcribe a string representing DNA nucleotides to RNA."
