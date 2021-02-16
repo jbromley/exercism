a059f94fd08741371099b279a251a9d9d9a38c95
J. Bromley <jbromley@gmail.com>
2021-02-16 01:18:05 -0800

    Completed the Luhn Common Lisp exercise.

--- common-lisp/luhn/luhn.lisp
+++ common-lisp/luhn/luhn.lisp
@@ -5,4 +5,24 @@
 
 (in-package #:luhn)
 
-(defun validp (input))
+(defun to-luhn-digit (n)
+  "Double the value of the digit. If the double value is greater than
+nine, then subtract nine from the doubled value."
+  (let ((n2 (* 2 n)))
+    (if (> n2 9)
+        (- n2 9)
+        n2)))
+
+(defun luhn-checksum (list)
+  "Calculate the Luhn checksum for the list of characters."
+  (loop
+    for (n1 n2) on (reverse list) by #'cddr
+    sum n1
+    when n2 sum (to-luhn-digit n2)))
+
+(defun validp (input)
+  "Check if a number has a valid Luhn checksum."
+  (let ((digits (map 'list #'digit-char-p
+                        (remove #\Space input))))
+    (when (and (> (length digits) 1) (not (member nil digits)))
+      (= 0 (mod (luhn-checksum digits) 10)))))
