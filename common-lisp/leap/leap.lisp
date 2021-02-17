6228d088d81aa75613f55ebf6788e4156bb4ecc2
J. Bromley <jay@appliedinvention.com>
2021-02-16 17:26:12 -0800

    Solved Common Lisp leap exercise.

--- common-lisp/leap/leap.lisp
+++ common-lisp/leap/leap.lisp
@@ -4,4 +4,7 @@
 (in-package #:leap)
 
 (defun leap-year-p (year)
-  )
+  "Return if YEAR is a leap year or not."
+  (or (and (= (mod year 4) 0)
+           (not (= (mod year 100) 0)))
+      (= (mod year 400) 0)))
