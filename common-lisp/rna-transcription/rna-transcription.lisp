74166fdc9c355effa73f0ba660f18a4d181891ab
J. Bromley <jay@appliedinvention.com>
2021-02-16 16:55:30 -0800

    Moved raising error to to-rna-base.
    Raise an error in to-rna-base when there is an invalid base. This
    allows reporting of the invalid base and simplifies both the
    to-rna-base and to-rna functions.

--- common-lisp/rna-transcription/rna-transcription.lisp
+++ common-lisp/rna-transcription/rna-transcription.lisp
@@ -10,13 +10,11 @@
                                    (#\T . #\A)))
 
 (defun to-rna-base (base)
-  "Transcribe a single DNA base to the RNA complement. Return NIL if the
-base is not a valid base (A, C, G, T)."
-  (cdr (assoc base +transcription-map+)))
+  "Transcribe a single DNA base to the RNA complement. Raise an error if
+the base is not a valid base (A, C, G, T)."
+  (or (cdr (assoc base +transcription-map+))
+        (error (format nil "invalid base: ~a" base))))
 
 (defun to-rna (str)
   "Transcribe a string representing DNA nucleotides to RNA."
-  (let ((transcription (map 'list #'to-rna-base str)))
-    (if (not (member nil transcription))
-        (coerce transcription 'string)
-        (error "invalid base"))))
+  (coerce (map 'list #'to-rna-base str) 'string))
