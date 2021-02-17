ba928dce86b0a01d4c4abe10377d57c6be6b9ee3
J. Bromley <jbromley@gmail.com>
2021-02-17 01:55:28 -0800

    Initial solution to Lisp anagram exercise.

--- common-lisp/anagram/anagram.lisp
+++ common-lisp/anagram/anagram.lisp
@@ -5,6 +5,22 @@
 
 (in-package #:anagram)
 
+(defun occurrences (str)
+  "Count the number occurrences of each element of the string STR.
+Return an alist sorted by the element."
+  (let* ((lst (coerce str 'list))
+         (elements (remove-duplicates lst))
+         (counts (pairlis elements (make-list (length elements) :initial-element 0))))
+    (loop for x in lst
+          do (incf (cdr (assoc x counts))))
+    (sort counts #'char< :key #'car)))
+
 (defun anagrams-for (subject candidates)
   "Returns a sublist of candidates which are anagrams of the subject."
-  )
+  (let* ((subject-lc (string-downcase subject))
+         (subject-freqs (occurrences subject-lc)))
+    (loop for candidate in candidates
+          for candidate-lc = (string-downcase candidate)
+          when (and (equal subject-freqs (occurrences candidate-lc))
+                    (not (equal subject-lc candidate-lc)))
+            collect candidate)))
