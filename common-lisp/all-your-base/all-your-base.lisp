ea3c6611d3eba1a145ac38cc83c30ef392118a67
J. Bromley <jbromley@gmail.com>
2021-02-18 02:42:21 -0800

    Wrote a from-base function for all-your base exercise.

--- common-lisp/all-your-base/all-your-base.lisp
+++ common-lisp/all-your-base/all-your-base.lisp
@@ -5,4 +5,15 @@
 
 (in-package #:all-your-base)
 
+(defun from-base (list-digits in-base)
+  "Convert LIST-DIGITS in IN-BASE into an integer."
+  (loop for digit in (reverse list-digits)
+        for index from 0
+        sum (* digit (expt in-base index))))
+
+(defun to-base (n out-base)
+  "Convert the number N into the digits in the number represented
+in base OUT-BASE."
+
+
 (defun rebase (list-digits in-base out-base))
