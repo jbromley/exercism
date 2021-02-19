659ce08b1efd7310fb29e50781afd74935f90f17
J. Bromley <jay@appliedinvention.com>
2021-02-18 18:03:21 -0800

    Added the Common Lisp acronym exercise.

---
+++ common-lisp/acronym/acronym.lisp
@@ -0,0 +1,10 @@
+(in-package #:cl-user)
+(defpackage #:acronym
+  (:use #:common-lisp)
+  (:export #:acronym))
+
+(in-package #:acronym)
+
+(defun acronym (str)
+  "Returns the acronym for a noun of tech jargon."
+  )
