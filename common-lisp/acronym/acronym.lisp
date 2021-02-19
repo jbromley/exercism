352aa062071f4f26de15536ca0dcaf3f9a903b27
J. Bromley <jay@appliedinvention.com>
2021-02-19 15:49:40 -0800

    Solved the Common Lisp acronym exercise.

--- common-lisp/acronym/acronym.lisp
+++ common-lisp/acronym/acronym.lisp
@@ -5,6 +5,19 @@
 
 (in-package #:acronym)
 
+(defun split-string (string &optional (delimiters '(#\space)))
+  "Split STRING into a list of words separated by the any character in the
+DELIMITERS list."
+  (loop for start = 0 then (1+ end)
+        as end = (position-if (lambda (ch) (member ch delimiters)) string :start start)
+        as substr = (subseq string start end)
+        when (string/= substr "") collect substr
+          while end))
+
+(defun first-char-upcase (str)
+  "Return the first character of STR in upper case."
+  (char-upcase (char str 0)))
+
 (defun acronym (str)
   "Returns the acronym for a noun of tech jargon."
-  )
+  (map 'string #'first-char-upcase (split-string str '(#\space #\-))))
