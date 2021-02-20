34b47e7509d7d9ea8876f97334536883ca05e919
J. Bromley <jbromley@gmail.com>
2021-02-20 01:31:43 -0800

    Added Common Lisp beer song exercise.

---
+++ common-lisp/beer-song/beer-song.lisp
@@ -0,0 +1,14 @@
+(in-package #:cl-user)
+(defpackage #:beer
+  (:use #:common-lisp)
+  (:export #:verse #:sing))
+
+(in-package #:beer)
+
+(defun verse (n)
+  "Returns a string verse for a given number."
+  )
+
+(defun sing (start &optional (end 0))
+  "Returns a string of verses for a given range of numbers."
+  )
