7885d3fa44c3269cfe0054f01c1b47f8ed9cd13c
J. Bromley <jay@appliedinvention.com>
2021-03-03 16:39:49 -0800

    Solved Common Lisp grains exercise.

--- common-lisp/grains/grains.lisp
+++ common-lisp/grains/grains.lisp
@@ -4,6 +4,10 @@
   (:export :square :total))
 (in-package #:grains)
 
-(defun square (n) )
+(defun square (n)
+  "Return the number of grains on the Nth square."
+  (expt 2 (1- n)))
 
-(defun total () )
+(defun total ()
+  "Return the total number of grains on the chessboard."
+  (loop for i from 1 to 64 sum (square i)))
