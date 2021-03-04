24fc9781c2ac3e628d92c95b3f0ae11f27e74d1d
J. Bromley <jay@appliedinvention.com>
2021-03-03 16:22:44 -0800

    Solved the Common Lisp sublist exercise.

--- common-lisp/sublist/sublist.lisp
+++ common-lisp/sublist/sublist.lisp
@@ -8,14 +8,12 @@
   "Determine if LIST1 is a sublist of LIST2. If LIST1 is longer than
 LIST2, NIL is returned."
   (unless (> (length list1) (length list2))
-    (let ((match-start (position (first list1) list2)))
+    (let ((match-start (position (first list1) list2 :start start)))
       (cond
         ((null match-start) nil)
-        (> (+ match-start (length list1)) (1- (length list2)) nil)  nil
-         (equal list1 (subseq list2 match-start (+ match-start (length list1)) start)) t
-         (t sublist-p list1 list2 match-start (1+ start))))))
-
-
+        ((> (+ match-start (length list1)) (length list2)) nil)
+        ((equal list1 (subseq list2 match-start (+ match-start (length list1)))) t)
+        (t (sublist-p list1 list2 (1+ match-start)))))))
 
 (defun sublist (list1 list2)
   "What is LIST1 of LIST2 (sublist, superlist, equal or unequal)"
