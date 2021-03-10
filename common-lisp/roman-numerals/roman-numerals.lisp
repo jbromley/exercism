2b9a2651b047bfc48a8926c8188f0926e7487a87
J. Bromley <jbromley@gmail.com>
2021-03-10 00:40:24 -0800

    Solved Common Lisp roman number exercise.

---
+++ common-lisp/roman-numerals/roman-numerals.lisp
@@ -0,0 +1,9 @@
+(defpackage #:roman
+  (:use #:cl)
+  (:export #:romanize))
+
+(in-package #:roman)
+
+(defun romanize (number)
+  "Returns the Roman numeral representation for a given number."
+  (format nil "~@r" number))
