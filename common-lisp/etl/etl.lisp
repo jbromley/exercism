210b2fbf4284b306fdd6ef66710b287f3ca9c92c
J. Bromley <jbromley@gmail.com>
2021-03-10 01:15:51 -0800

    Added Common Lisp ETL exercise.

---
+++ common-lisp/etl/etl.lisp
@@ -0,0 +1,10 @@
+(in-package #:cl-user)
+(defpackage #:etl
+  (:use #:common-lisp)
+  (:export #:transform))
+
+(in-package #:etl)
+
+(defun transform (data)
+  "Transforms hash values into keys with their keys as their values."
+  )
