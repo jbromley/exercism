ebd3bf2605a8317c9a0de3bc66b87b06e6388c24
J. Bromley <jbromley@gmail.com>
2021-03-13 00:53:42 -0800

    Added a second tranform function.
    Loop over hash table with hash-values and hash-key. Use a with clause to
    define the hash table that is returned.

--- common-lisp/etl/etl.lisp
+++ common-lisp/etl/etl.lisp
@@ -12,3 +12,9 @@
       (loop for key in (gethash value data)
             do (setf (gethash (string-downcase key) h) value)))
     h))
+
+(defun transform-2 (data)
+  (loop for keys being the hash-values in data using (hash-key value)
+        with h = (make-hash-table :test 'equalp)
+        do (loop for key in keys do (setf (gethash (string-downcase key) h) value))
+           finally (return h)))
