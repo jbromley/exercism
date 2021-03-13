72ec25f53d57c4852c8d317d62757a00fd991137
J. Bromley <jbromley@gmail.com>
2021-03-13 00:33:46 -0800

    Solved Common Lisp ETL exercise.

--- common-lisp/etl/etl.lisp
+++ common-lisp/etl/etl.lisp
@@ -7,4 +7,8 @@
 
 (defun transform (data)
   "Transforms hash values into keys with their keys as their values."
-  )
+  (let ((h (make-hash-table :test 'equalp)))
+    (loop for value being the hash-keys of data do
+      (loop for key in (gethash value data)
+            do (setf (gethash (string-downcase key) h) value)))
+    h))
