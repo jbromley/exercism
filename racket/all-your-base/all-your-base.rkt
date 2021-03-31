b212845fe2679f7763e37aef042a1f5dfaa00c3c
J. Bromley <jay@appliedinvention.com>
2021-03-31 16:50:17 -0700

    Started work on Racket all-your-base exercise.

--- racket/all-your-base/all-your-base.rkt
+++ racket/all-your-base/all-your-base.rkt
@@ -2,4 +2,16 @@
 
 (provide rebase)
 
-(define (rebase list-digits in-base out-base))
+(require predicates)
+
+(define (valid-digit? digit base)
+  (and (>= digit 0) (< digit base)))
+
+(define (valid-digits? digits base)
+  (all? valid-digit?))
+
+; (define (from-base digit-list in-base)
+
+
+(define (rebase list-digits in-base out-base)
+  #f)
