c3860cab9bc6342ba84a8f978eafefb13475733c
J. Bromley <jay@appliedinvention.com>
2023-03-11 01:24:53 -0800

    Solve RNA transcription using `traverse`.

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -1,11 +1,11 @@
 module DNA (toRNA) where
 
 toRNA :: String -> Either Char String
-toRNA "" = Right ""
-toRNA (x:xs)
-    | x == 'A' = ("U" ++) <$> toRNA xs
-    | x == 'T' = ("A" ++) <$> toRNA xs
-    | x == 'C' = ("G" ++) <$> toRNA xs
-    | x == 'G' = ("C" ++) <$> toRNA xs
-    | otherwise = Left x
-          
+toRNA = traverse fromDNA
+    where 
+        fromDNA :: Char -> Either Char Char
+        fromDNA 'A' = pure 'U'
+        fromDNA 'T' = pure 'A'
+        fromDNA 'C' = pure 'G'
+        fromDNA 'G' = pure 'C'
+        fromDNA ch = Left ch
