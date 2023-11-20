7013527213365df8f2958a059866cb070acb21bb
J. Bromley <jay@appliedinvention.com>
2023-11-20 13:33:03 -0800

    Simplified `cond` logic in `to_label`
    Don't have to specifically handle legacy messages for trace and fatal.
    Since they don't match any other clause, they will fall through to the
    true branch that returns `:unknown`.

--- elixir/log-level/lib/log_level.ex
+++ elixir/log-level/lib/log_level.ex
@@ -1,13 +1,11 @@
 defmodule LogLevel do
   def to_label(level, legacy?) do
     cond do
-      level == 0 and legacy? -> :unknown
       level == 0 and not legacy? -> :trace
       level == 1 -> :debug
       level == 2 -> :info
       level == 3 -> :warning
       level == 4 -> :error
-      level == 5 and legacy? -> :unknown
       level == 5 and not legacy? -> :fatal
       true -> :unknown
     end
