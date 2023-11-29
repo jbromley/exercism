1b208dcdefdeae5b8a9e0a5605570017c30eb236
J. Bromley <jay@appliedinvention.com>
2023-11-29 13:50:44 -0800

    Solved Protein Translation in Elixir
    Exercism claims the problem was changed, but I don't appear to have
    solved this previously.

--- elixir/protein-translation/lib/protein_translation.ex
+++ elixir/protein-translation/lib/protein_translation.ex
@@ -1,9 +1,47 @@
 defmodule ProteinTranslation do
+  @codon_map %{
+    "UGU" => "Cysteine",
+    "UGC" => "Cysteine",
+    "UUA" => "Leucine",
+    "UUG" => "Leucine",
+    "AUG" => "Methionine",
+    "UUU" => "Phenylalanine",
+    "UUC" => "Phenylalanine",
+    "UCU" => "Serine",
+    "UCC" => "Serine",
+    "UCA" => "Serine",
+    "UCG" => "Serine",
+    "UGG" => "Tryptophan",
+    "UAU" => "Tyrosine",
+    "UAC" => "Tyrosine",
+    "UAA" => "STOP",
+    "UAG" => "STOP",
+    "UGA" => "STOP",
+    "" => "STOP"
+  }
+
   @doc """
   Given an RNA string, return a list of proteins specified by codons, in order.
   """
-  @spec of_rna(String.t()) :: {atom, list(String.t())}
+  @spec of_rna(String.t()) :: {:ok, list(String.t())} | {:error, String.t()}
   def of_rna(rna) do
+    of_rna(rna, [])
+  end
+
+  @spec of_rna(String.t(), list(String.t())) :: {:ok, list(String.t())} | {:error, String.t()}
+  defp of_rna(rna, aminos) do
+    {codon, rna} = String.split_at(rna, 3)
+
+    case of_codon(codon) do
+      {:error, _} ->
+        {:error, "invalid RNA"}
+
+      {:ok, amino} when amino == "STOP" ->
+        {:ok, Enum.reverse(aminos)}
+
+      {:ok, amino} ->
+        of_rna(rna, [amino | aminos])
+    end
   end
 
   @doc """
@@ -27,7 +65,11 @@ defmodule ProteinTranslation do
   UAG -> STOP
   UGA -> STOP
   """
-  @spec of_codon(String.t()) :: {atom, String.t()}
+  @spec of_codon(String.t()) :: {:ok, String.t()} | {:error, String.t()}
   def of_codon(codon) do
+    case Map.get(@codon_map, codon) do
+      nil -> {:error, "invalid codon"}
+      amino -> {:ok, amino}
+    end
   end
 end
