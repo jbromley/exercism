2d5fd01ba17d1d08776aa7abc772f00ca27a1970
J. Bromley <jay@appliedinvention.com>
2023-12-01 19:30:39 -0800

    Solved Collatz Conjecture in Erlang

---
+++ erlang/collatz-conjecture/src/collatz_conjecture.erl
@@ -0,0 +1,8 @@
+-module(collatz_conjecture).
+
+-export([steps/1]).
+
+steps(N) when N =< 0 -> error(badarg);
+steps(1) -> 0;
+steps(N) when N rem 2 == 0 -> 1 + steps(N div 2);
+steps(N) when N rem 2 == 1 -> 1 + steps(3 * N + 1).
