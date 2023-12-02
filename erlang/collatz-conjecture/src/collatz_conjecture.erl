f97acfd8de95d69811136b845b26c67de90e592d
J. Bromley <jay@appliedinvention.com>
2023-12-01 19:35:29 -0800

    Make Erlang Collatz Conjecture tail-recursive

--- erlang/collatz-conjecture/src/collatz_conjecture.erl
+++ erlang/collatz-conjecture/src/collatz_conjecture.erl
@@ -2,7 +2,10 @@
 
 -export([steps/1]).
 
-steps(N) when N =< 0 -> error(badarg);
-steps(1) -> 0;
-steps(N) when N rem 2 == 0 -> 1 + steps(N div 2);
-steps(N) when N rem 2 == 1 -> 1 + steps(3 * N + 1).
+steps(N) when is_integer(N), N > 0 -> steps(N, 0);
+steps(_) -> error(badarg).
+
+steps(1, Result) -> Result;
+steps(N, Result) when N rem 2 == 0 -> steps(N div 2, Result + 1);
+steps(N, Result) -> steps(3 * N + 1, Result + 1).
+
